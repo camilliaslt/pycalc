6a074b5d360d062fe546269befa31ad65af79b91
Soltani <soltani.camillia@gmail.com>
2026-03-29 20:19:29 +0200

    soustraction

--- pycalc.py
+++ pycalc.py
@@ -6,6 +6,8 @@ def calculatrice(x:float, y:float, signe:str):
     
     if (signe == '+'):
         print(x + y)
+     elif (signe == '-'):
+        print(x - y)
 
 x = float(sys.argv[1])
 signe = sys.argv[2]
