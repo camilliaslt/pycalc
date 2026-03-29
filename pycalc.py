e5b8a582b26ef3959b9b6924467b9b3d9aa77822
Soltani <soltani.camillia@gmail.com>
2026-03-29 20:18:07 +0200

    addition

---
+++ pycalc.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.argv)
+
+def calculatrice(x:float, y:float, signe:str):
+    
+    if (signe == '+'):
+        print(x + y)
+
+x = float(sys.argv[1])
+signe = sys.argv[2]
+y = float(sys.argv[3])
+calculatrice(x,y,signe)
